248e415c8fc00402682cad1d4eb7eb126b1b52e3
Robert Facella <rob.facella@gmail.com>
2019-02-24 13:56:18 -0500

    Login.php to-do

---
+++ WWW/login.php
@@ -0,0 +1,19 @@
+<?php
+
+//login with provide credentials if in the db
+
+$username = $_POST['user'];
+$password = $_POST['pass];
+
+//Sanitize data to prevent SQLInjection
+
+//connect to database server and select Accounts table
+
+//Query DB for user and log them in if credentials are correct
+if
+
+else{
+	echo "Login ERROR: Invalid Credentials."
+}
+
+?>
