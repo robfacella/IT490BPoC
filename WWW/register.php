9dcac455163a6ddd0922f124dc948ff0b10d4ef4
Robert Facella <rob.facella@gmail.com>
2019-02-24 13:49:45 -0500

    Registration.php to-do.

---
+++ WWW/register.php
@@ -0,0 +1,24 @@
+<?php
+	$user;
+	$email;
+	$pass;
+	$confPass;
+
+	//Connect to db
+	
+	//On reg button click
+	//Check fields are set
+	//Verify Password == confirmed password
+	//if{
+
+	//}
+
+	//hash password before storing
+
+	//sql statement to store data
+
+	//sql query with statement and db
+
+
+
+?>
