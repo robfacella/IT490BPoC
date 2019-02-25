ebe771ca835d6bcb5d21d58583202e27ae697bd4
Robert Facella <rob.facella@gmail.com>
2019-02-25 15:10:37 -0500

    I was DISGUSTINGLY wrong about thinking it would be easier to reconfigure the apache server to run with part of this Repository as its root. Resorting to copying files from here to /var/www/html

---
+++ WWW/html/testConnect.php
@@ -0,0 +1,7 @@
+<?php
+//Can We Connect via PHP
+include("dbAccount.php");
+mysqli_connect($hostname, $username, $password, $database) or die (mysqli_error());
+print "Connected.\n";
+
+?>
--- apache2Setup.sh
+++ apache2Setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 apt-get install apache2 -y
-
+apt-get install libapache2-mod-php7.0 -y
+#php7.0-curl && ...-json   ??Maybe??
 
 wordir=$(pwd) 
-echo "use: nano /etc/apache2/sites-available/000-default.conf"
-echo "Change: /var/www/html"
-echo "To: "
-echo "$wordir""/WWW/html"
+##echo "use: nano /etc/apache2/sites-available/000-default.conf"
 
+echo "Copying files fromthis repository's WWW/html directory to /var/www/html"
+cp -r WWW/html/. /var/www/html
+echo "Copied..."
 
+echo "Restarting apache2 server..."
+service apache2 restart
+echo "DONE."
