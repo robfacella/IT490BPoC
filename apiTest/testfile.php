a1e2773bcd1fcbb91bcd2fd733fd72db3eca8bd3
Conro101 <cbot.596@gmail.com>
2019-03-05 12:42:39 -0500

    Successfully pulled and displayed API info

--- apiTest/testfile.php
+++ apiTest/testfile.php
@@ -2,5 +2,5 @@
 
 // your code goes here
 print("hello world");
-print_r(json_decode("http://www.omdbapi.com/?t=Blade%20Runner&apikey=f0530b1d"), true);
+print_r(json_decode(file_get_contents("http://www.omdbapi.com/?t=Blade%20Runner&apikey=f0530b1d"), true));
 ?>
