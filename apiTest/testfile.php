b3d9a7713b7453e6feadff10d113a2f77627c290
Conro101 <cbot.596@gmail.com>
2019-03-06 16:03:28 -0500

    Gets movie title from command line & returns year

--- apiTest/testfile.php
+++ apiTest/testfile.php
@@ -1,11 +1,20 @@
 <?php
 
 // your code goes here
-$movieInfo = json_decode(file_get_contents("http://www.omdbapi.com/?t=The%20Good%20Place&apikey=f0530b1d"), true);
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "Blade Runner";
+}
+
+$movieInfo = json_decode(file_get_contents("http://www.omdbapi.com/?t=" . $msg . "&apikey=f0530b1d"), true);
 print_r($movieInfo);
 if ($movieInfo["Type"] == "movie") {
   print("This is a movie and we can proceed".PHP_EOL);
-  print($movieInfo["Title"]);
+  print($movieInfo["Year"]);
+  //INSERT INTO Movies (Title, Year, Rated, Genre, Director, Actors, Poster) VALUES ($movieInfo["Title"], $movieInfo["Year"], $movieInfo["Rated"], $movieInfo["Genre"], $movieInfo["Director"], $movieInfo["Actors"], $movieInfo["Poster"])
 }
-//INSERT INTO Movies (Title, Year, Rated, Genre, Director, Actors, Poster) VALUES ($movieInfo["Title"], $movieInfo["Year"], $movieInfo["Rated"], $movieInfo["Genre"], $movieInfo["Director"], $movieInfo["Actors"], $movieInfo["Poster"])
 ?>
