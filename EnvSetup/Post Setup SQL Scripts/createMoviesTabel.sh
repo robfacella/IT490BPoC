7197680d71d84233aa2727d0634f7c328c039f1c
ajk64 <47427443+ajk64@users.noreply.github.com>
2019-03-06 17:17:10 -0500

    Create createMoviesTabel.sh

---
+++ EnvSetup/Post Setup SQL Scripts/createMoviesTabel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#ROOT
+#mysql -u root -proot <<EOF
+#TestUser
+mysql -u testuser -ppassword <<EOF
+USE testdb;
+#SHOW tables;
+#Uncomment if TableName already exists.
+#DROP TABLE movies;
+Create table movies (
+	movieid int not null primary key auto_increment,
+	year int not null,
+	rated varchar(255) not null,
+	released int not null,
+  genre varchar(255) not null,
+  actors varchar(255) not null,
+  poster varchar(255) not null,
+);
+show columns from movies;
+EOF
