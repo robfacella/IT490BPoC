805ba4af460473e25f0a172c5722e8db45074a01
Conro101 <cbot.596@gmail.com>
2019-03-06 16:04:16 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/robfacella/IT490BPoC""
    This reverts commit 9d112bf2d312c6463b6d0cbe9258305c747c343a.

--- Rabbit/testRabbitMQClient.php
+++ Rabbit/testRabbitMQClient.php
@@ -19,11 +19,12 @@ $request['type'] = "test"; //Should interact with Server.php's Switch-Statement
 $request['username'] = "test";
 $request['password'] = "test";
 $request['message'] = $msg;
-$response = $client->send_request($request); //(Response)
+$response = $client->send_request($request); //(Response) //$response comes from RabbitMQServer.php 's Switch-Statement.
 //$response = $client->publish($request); //One-Way (NO response).
 
+//What does our Client DO with the $response ?
 echo "client received response: ".PHP_EOL;
-print_r($response);
+print_r($response);  
 echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
