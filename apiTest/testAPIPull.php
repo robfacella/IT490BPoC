5357ec88a2e3748c67a1d3455e074ebe9cbed045
Conro101 <cbot.596@gmail.com>
2019-03-06 16:16:37 -0500

    Now doesn't ruin everything if the search fails!

--- apiTest/testAPIPull.php
+++ apiTest/testAPIPull.php
@@ -12,9 +12,11 @@ else
 
 $movieInfo = json_decode(file_get_contents("http://www.omdbapi.com/?t=" . $msg . "&apikey=f0530b1d"), true);
 print_r($movieInfo);
-if ($movieInfo["Type"] == "movie") {
-  print("This is a movie and we can proceed".PHP_EOL);
-  print($movieInfo["Year"]);
-  //INSERT INTO Movies (Title, Year, Rated, Genre, Director, Actors, Poster) VALUES ($movieInfo["Title"], $movieInfo["Year"], $movieInfo["Rated"], $movieInfo["Genre"], $movieInfo["Director"], $movieInfo["Actors"], $movieInfo["Poster"])
+if($movieInfo["Response"] == "True"){
+  if ($movieInfo["Type"] == "movie") {
+    print("This is a movie and we can proceed".PHP_EOL);
+    print($movieInfo["Year"]);
+    //INSERT INTO Movies (Title, Year, Rated, Genre, Director, Actors, Poster) VALUES ($movieInfo["Title"], $movieInfo["Year"], $movieInfo["Rated"], $movieInfo["Genre"], $movieInfo["Director"], $movieInfo["Actors"], $movieInfo["Poster"])
+  }
 }
 ?>
