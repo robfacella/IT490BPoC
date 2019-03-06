7e0c432daa3edcd86a302e12d981740d42edeb61
Conro101 <cbot.596@gmail.com>
2019-03-06 15:54:22 -0500

    Now checks to make sure that it IS a movie and outputs the title if it is

--- apiTest/testfile.php
+++ apiTest/testfile.php
@@ -1,6 +1,11 @@
 <?php
 
 // your code goes here
-print("hello world".PHP_EOL);
-print_r(json_decode(file_get_contents("http://www.omdbapi.com/?t=Blade%20Runner&apikey=f0530b1d"), true));
+$movieInfo = json_decode(file_get_contents("http://www.omdbapi.com/?t=The%20Good%20Place&apikey=f0530b1d"), true);
+print_r($movieInfo);
+if ($movieInfo["Type"] == "movie") {
+  print("This is a movie and we can proceed".PHP_EOL);
+  print($movieInfo["Title"]);
+}
+//INSERT INTO Movies (Title, Year, Rated, Genre, Director, Actors, Poster) VALUES ($movieInfo["Title"], $movieInfo["Year"], $movieInfo["Rated"], $movieInfo["Genre"], $movieInfo["Director"], $movieInfo["Actors"], $movieInfo["Poster"])
 ?>
