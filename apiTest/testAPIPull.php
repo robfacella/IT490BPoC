42d1347547af83ff103e44c271cb01deaf9d405d
Conro101 <cbot.596@gmail.com>
2019-03-07 12:18:47 -0500

    Fixed API to work in linux

--- apiTest/testAPIPull.php
+++ apiTest/testAPIPull.php
@@ -1,3 +1,4 @@
+#!/usr/bin/php
 <?php
 
 // your code goes here
@@ -16,8 +17,8 @@ if($movieInfo["Response"] == "True"){
   if ($movieInfo["Type"] == "movie") {
     print("This is a movie and we can proceed".PHP_EOL);
     print($movieInfo["Year"]);
-    $serverInfo = mysqli_connect("128.235.159.32", "testuser", "password", "testdb");
-    $SQLquery = "INSERT INTO Movies (Title, Year, Rated, Released, Genre, Director, Actors, Poster) VALUES (".$movieInfo["Title"].", ".$movieInfo["Year"].", ".$movieInfo["Rated"].", ".$movieInfo["Released"].", ".$movieInfo["Genre"].", ".$movieInfo["Director"].", ".$movieInfo["Actors"].", ".$movieInfo["Poster"].")";
+    $serverInfo = mysqli_connect("192.168.8.235", "testuser", "password", "testdb");
+    $SQLquery = "INSERT INTO movies(Title, Year, Rated, Released, Genre, Director, Actors, Poster) VALUES (".$movieInfo["Title"].", ".$movieInfo["Year"].", ".$movieInfo["Rated"].", ".$movieInfo["Released"].", ".$movieInfo["Genre"].", ".$movieInfo["Director"].", ".$movieInfo["Actors"].", ".$movieInfo["Poster"].")";
     mysql_query($SQLquery);
     $serverInfo->close;
   }
