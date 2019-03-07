ad021c9330508fab28a78cf50178ef51b4f0f056
Conro101 <cbot.596@gmail.com>
2019-03-07 12:44:01 -0500

    Little checks to get server going

--- apiTest/testAPIPull.php
+++ apiTest/testAPIPull.php
@@ -17,8 +17,9 @@ if($movieInfo["Response"] == "True"){
   if ($movieInfo["Type"] == "movie") {
     print("This is a movie and we can proceed".PHP_EOL);
     print($movieInfo["Year"]);
-    $serverInfo = mysqli_connect("192.168.8.235", "testuser", "password", "testdb");
+    $serverInfo = mysqli_connect("127.0.0.1", "root", "root", "testdb") or die (mysqli_error());
     $SQLquery = "INSERT INTO movies(Title, Year, Rated, Released, Genre, Director, Actors, Poster) VALUES (".$movieInfo["Title"].", ".$movieInfo["Year"].", ".$movieInfo["Rated"].", ".$movieInfo["Released"].", ".$movieInfo["Genre"].", ".$movieInfo["Director"].", ".$movieInfo["Actors"].", ".$movieInfo["Poster"].")";
+    echo $SQLquery;
     mysql_query($SQLquery);
     $serverInfo->close;
   }
