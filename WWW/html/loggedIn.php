9868ddcebef02852454d9b3d381cf9eac2c64e21
Robert Facella <rob.facella@gmail.com>
2019-03-11 13:18:01 -0400

    or ||

--- WWW/html/loggedIn.php
+++ WWW/html/loggedIn.php
@@ -5,7 +5,7 @@ require_once('rabbitMQLib.inc');
   session_start();
   //Need to work on Validation through rabbitMQ
   //if either Session 'uname' or 'pwo' are unset then we don't even need to ccheck validation because it cannot be a valid session.
-  if((!isset($_SESSION['uname'])  or (!isset($_SESSION['pwo']))
+  if((!isset($_SESSION['uname'])  || (!isset($_SESSION['pwo']))
   {   //Redirect to login page
       session_destroy(); //Will remove other session information if it was set somehow.
       echo "<script>location.href='login.php'</script>";
