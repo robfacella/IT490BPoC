ab0d02e6426f067f91517b7abcad344725181bbe
Robert Facella <rob.facella@gmail.com>
2019-03-23 16:27:58 -0400

    Logfile Template.

--- LogFile/Time.php
+++ LogFile/Time.php
@@ -3,4 +3,6 @@
 echo ' Day Now:    '. date('Y-m-d') ."\n";
 echo 'Time Now:    '. date('H:i:s:v-T') ."\n";
 
+echo "Logfile Template: \n";
+echo 'Date['. date('Y-m-d') .'] Time['. date('H:i:s') .']: '."MESSAGE HERE\n";
 ?>
