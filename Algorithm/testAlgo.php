c59b38c1a062e3af843c7eeafc1c59ceeb4056d6
Conro101 <cbot.596@gmail.com>
2019-03-24 16:08:14 -0400

    Mostly done besides database integration

--- Algorithm/testAlgo.php
+++ Algorithm/testAlgo.php
@@ -1,3 +1,4 @@
+#!/bin/php
 <?php
   //Here is where we initalize our fake users
   //initalize user stats
@@ -8,6 +9,7 @@
   //suggest movie closest to stats
 
   class userProfile {
+    public $userID;
     public $statNames = array('Genre', 'Director', 'Actors');
     public $weighedStats = array('Genre'=> array(), 'Director'=>array(),/* 'Year'=>array(),*/ 'Actors'=>array());
     public $unweighedStats = array('Genre'=> array(), 'Director'=>array(),/* 'Year'=>array(),*/ 'Actors'=>array());
@@ -17,6 +19,9 @@
     //functions to set info from database
     public function setWeighedStats($userStats){
       $weighedStats = $userStats;
+    }
+    public function setUserID($inputID){
+      $userID = $inputID;
     }
     public function setMovieList($userList){
       $movieList = $userList;
@@ -64,7 +69,102 @@
     }
   }
 
-  //create temp user for current session
+  function recommendMovie($usersToWatch, $otherUsers){
+    $statNames = array('Genre', 'Director', 'Actors');
+    $groupStatsWeighed = array('Genre'=> array(), 'Director'=>array(),/* 'Year'=>array(),*/ 'Actors'=>array());
+    $groupStatsUnweighed = array('Genre'=> array(), 'Director'=>array(),/* 'Year'=>array(),*/ 'Actors'=>array());
+    $maxStatName = array();
+    $maxStatNumber = array(0, 0, 0);
+    $matchingUsers = array();
+
+    //same as adding movies just with multiple users
+    foreach ($usersToWatch as $userAdding) {
+      foreach($statNames as $currentStat){
+        //iterate through all genres to add
+        foreach($groupStatsUnweighed[$currentStat] as &$oldStat){
+          foreach($userAdding as $userStat){
+            //check if genre already exists in user stats
+            //if so, add to existing array
+            if ($oldStat[0] == $userStat[0]){
+              $oldStat[1] = $oldStat[1] + $userStat[1];
+            }
+            else{
+              //genre doesn't exist in stats, add
+              array_push($groupStatsUnweighed[$currentStat], array($userStat[0] , $userStat[]));
+            }
+          }
+          //clear weighed stats' original array
+          unset($groupStatsWeighed[$currentStat]);
+          //set weighed to unweighed
+          $groupStatsWeighed[$currentStat] = $groupStatsUnweighed[$currentStat];
+          //iterate through entries
+          foreach ($groupStatsWeighed[$currentStat] as &$valueToBeWeighed) {
+            //average entries
+            $valueToBeWeighed[1] = $valueToBeWeighed[1]/count($usersToWatch);
+          }
+          unset($valueToBeWeighed);
+        }
+        unset($oldStat);
+      }
+    }
+
+    //Here is where database stuff will come into play.
+    $iterationCount = 0;
+    foreach ($statNames as $currentStat) {
+      foreach ($groupStatsWeighed[$currentStat] as $currentSort) {
+        //check if next value is higher
+        if($currentSort[1] > $maxStatNumber[$iterationCount]){
+          $maxStatName[$iterationCount] = $currentSort[0];
+          $maxStatNumber[$iterationCount] = $currentSort[1];
+        }
+        //if two values are the same, randomly pick one to keep
+        elseif ($currentSort[1] = $maxStatNumber[$iterationCount]) {
+          $rando = rand(0,1);
+          if($rando = 1){
+            $maxStatName[$iterationCount] = $currentSort[0];
+            $maxStatNumber[$iterationCount] = $currentSort[1];
+          }
+        }
+      }
+      $iterationCount++;
+    }
+
+    $x = 0;
+    foreach ($otherUsers as $currentOther) {
+      $otherStatName = array();
+      $otherStatName = array(0, 0, 0);
+      $iterationCount = 0;
+      foreach ($statNames as $currentStat) {
+        foreach ($currentOther->$weighedStats[$currentStat] as $currentSort) {
+          //check if next value is higher
+          if($currentSort[1] > $otherStatNumber[$iterationCount]){
+            $otherStatName[$iterationCount] = $currentSort[0];
+            $otherStatNumber[$iterationCount] = $currentSort[1];
+          }
+          //if two values are the same, randomly pick one to keep
+          elseif ($currentSort[1] = $otherStatNumber[$iterationCount]) {
+            $rando = rand(0,1);
+            if($rando = 1){
+              $otherStatName[$iterationCount] = $currentSort[0];
+              $otherStatNumber[$iterationCount] = $currentSort[1];
+            }
+          }
+        }
+        $iterationCount++;
+
+        if($otherStatName[0] == $maxStatName[1]){
+          $matchingUsers[$x] = $currentOther->$userID;
+          $x++;
+        }
+      }
+    }
+
+    //Once we have a selected user(s), go through the database and pick out all movies
+    //that have max genre, remove any already seen,
+    //then pick one randomly
+  }
+
+/*  //create temp user for current session
   $currentUser = new userProfile();
   $looper = True;
   //check if button pressed
@@ -87,7 +187,5 @@
         }
       }
     }
-}
+}*/
 ?>
-</body>
-</html>
