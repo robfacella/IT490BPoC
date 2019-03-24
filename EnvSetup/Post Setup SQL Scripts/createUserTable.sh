3b96e68c6599510560fda6afd095bed2a9e8d584
ajk64 <47427443+ajk64@users.noreply.github.com>
2019-03-24 15:36:36 -0400

    Update createUserTable.sh
    added relationship table. look at https://www.codedodle.com/2014/12/social-network-friends-database.html for source and additional info such as SQL commands and useful PHP

--- EnvSetup/Post Setup SQL Scripts/createUserTable.sh
+++ EnvSetup/Post Setup SQL Scripts/createUserTable.sh
@@ -17,8 +17,25 @@ Create table users (
 	username varchar(255) not null,
 	email varchar(255) not null,
 	password varchar(255) not null
-);
+); ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#friends table
+#SQL commands and PHP found here https://www.codedodle.com/2014/12/social-network-friends-database.html
+CREATE TABLE IF NOT EXISTS `relationship` (
+  user_one_id INT(10) UNSIGNED NOT NULL,
+  user_two_id INT(10) UNSIGNED NOT NULL,
+  status TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+  action_user_id INT(10) UNSIGNED NOT NULL,
+  FOREIGN KEY (`user_one_id`) REFERENCES users(`userid`),
+  FOREIGN KEY (`user_two_id`) REFERENCES users(`userid`),
+  FOREIGN KEY (`action_user_id`) REFERENCES users(`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `relationship`
+ADD UNIQUE KEY `unique_users_id` (`user_one_id`,`user_two_id`);
+
 show columns from users;
+show columns from relationship;
 
 EOF
 
