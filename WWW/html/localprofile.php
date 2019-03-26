f8a92202a727ad9a2a524532475444eef4780f9c
Michael Pabian <michaelpabian@gmail.com>
2019-03-26 14:32:29 -0400

    Created a local backup file

---
+++ WWW/html/localprofile.php
@@ -0,0 +1,70 @@
+<?php
+//pulls the profile name from the url
+$user = $_GET["user"];
+//this is set up to get data from a local database, needs to be changed to work with rabbit
+$db = mysqli_connect("localhost", "root","", "testdb"); 
+if (mysqli_connect_errno())
+  {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  exit();
+  }
+mysqli_select_db($db, "testdb" ); 
+//error reporting
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+ini_set( 'display_errors' , 1 );
+
+//pull user data
+$s = "select * from accounts where username = '$user' " ;
+($t = mysqli_query($db, $s) ) or die ( mysqli_error( $db ) );
+while ( $r = mysqli_fetch_array ( $t, MYSQLI_ASSOC) ) {
+	$movies = $r[ "movies" ];
+	$friends = $r[ "friends" ];
+}
+?>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+	<title>Profile</title>
+</head>
+
+<body>
+	<div class="container">
+	<h1><?php echo $user; ?>'s Profile<h1><br>
+	<form method="post" action="">
+		Insert Profile jargon here <br>
+		
+		<form method="post" action"">
+		Movie list: <?php echo $movies; ?> <br>
+		Add watched movies <input type="text" name="newMovie"> <br>
+		<input type="submit" value="submit" name="msubmit_btn"> <br> <br>
+		</form>
+		
+		<form method="post" action"">
+		Friends list: <?php echo $friends; ?> <br>
+		Add friends <input type="text" name="newFriend"> <br>
+		<input type="submit" value="submit" name="fsubmit_btn"> <br> <br>
+		</form>
+		
+	</form>
+	</div>
+</body>
+
+</html>
+<?php 
+if(isset($_REQUEST['msubmit_btn']))
+{
+	$newMovie =  $_POST["newMovie"];
+	$movies = $movies . ", " . $newMovie;
+	$s = "update accounts set movies = '$movies' where username = '$user' ";
+	($t = mysqli_query($db, $s) ) or die ( mysqli_error( $db ) );
+}
+if(isset($_REQUEST['fsubmit_btn']))
+{
+	$newFriend =  $_POST["newFriend"];
+	$friends = $friends . ", " . $newFriend;
+	$s = "update accounts set friends = '$friends' where username = '$user' ";
+	($t = mysqli_query($db, $s) ) or die ( mysqli_error( $db ) );
+}
+?>
