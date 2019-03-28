0b92ff58160201ff222840002ed65d726455e75a
ajk64 <47427443+ajk64@users.noreply.github.com>
2019-03-27 20:49:31 -0400

    changes

--- EnvSetup/Post Setup SQL Scripts/createRatingsTable.sh
+++ EnvSetup/Post Setup SQL Scripts/createRatingsTable.sh
@@ -13,9 +13,9 @@ CREATE TABLE IF NOT EXISTS ratings (
     userid varchar(255) NOT NULL,
     genre varchar(255) NOT NULL,
     rating int(11),
-    FOREIGN KEY (userid) REFERENCES users(userid), 
-    FOREIGN KEY (genre) REFERENCES movies(genre),
     PRIMARY KEY (userid, genre)
+    CONSTRAINT FK_user FOREIGN KEY (userid) REFERENCES users(userid), 
+    CONSTRAINT FK_genre FOREIGN KEY (genre) REFERENCES movies(genre),
 );
 
 show columns from ratings;
