bf6eb440d6fbf2505192b32a81a8a3c848df9591
ajk64 <47427443+ajk64@users.noreply.github.com>
2019-03-27 21:01:22 -0400

    primary key

--- EnvSetup/Post Setup SQL Scripts/createRatingsTable.sh
+++ EnvSetup/Post Setup SQL Scripts/createRatingsTable.sh
@@ -13,6 +13,7 @@ CREATE TABLE IF NOT EXISTS ratings (
     userid varchar(255) NOT NULL,
     genre varchar(255) NOT NULL,
     rating int(11),
+    PRIMARY KEY (userid, genre),
     CONSTRAINT FK_user FOREIGN KEY (userid) REFERENCES users(userid), 
 );
 
