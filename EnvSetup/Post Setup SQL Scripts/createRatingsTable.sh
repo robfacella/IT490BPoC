72584026540b58f70967c78504901d34f4c8849e
ajk64 <47427443+ajk64@users.noreply.github.com>
2019-03-27 21:08:36 -0400

    int not varchar

--- EnvSetup/Post Setup SQL Scripts/createRatingsTable.sh
+++ EnvSetup/Post Setup SQL Scripts/createRatingsTable.sh
@@ -10,7 +10,7 @@ USE testdb;
 
 #rating table
 CREATE TABLE IF NOT EXISTS ratings (
-    userid varchar(255) NOT NULL,
+    userid int(10) NOT NULL,
     genre varchar(255) NOT NULL,
     rating int(11),
     PRIMARY KEY (userid, genre),
