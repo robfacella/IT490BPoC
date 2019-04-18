aae5ecff0b7513369f8f8202bf01299c4f26a368
Robert Facella <rob.facella@gmail.com>
2019-04-18 11:56:33 -0400

    >>
    Big Yikes

--- EnvSetup/AddSecondInterface.sh
+++ EnvSetup/AddSecondInterface.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Hope you didn't forget to change the Static IP before running this. LMAO"
-bash AddSecondInterfaceInfo.sh > /etc/network/interfaces
+bash AddSecondInterfaceInfo.sh >> /etc/network/interfaces
