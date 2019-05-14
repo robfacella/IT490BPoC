e8e8325d7977edc468e8f4ac8f37518abae31932
Robert Facella <rob.facella@gmail.com>
2019-05-14 18:41:33 -0400

    Update register.php

--- WWW/html/register.php
+++ WWW/html/register.php
@@ -13,22 +13,8 @@ body {
 }
 </style>
 </head>
-<script>
-var pword = document.getElementById("passwd");
-var cpswd = document.getElementById("confPass");
-//Test for matching password fields.//Verify Password == confirmed password
-function checkPasses(){
 
-	if(pword.value != cpswd.value){
-		document.getElementById("nonConf").style.display="block";
-		cpswd.value="";
-	}if(pword.value == cpswd.value){
-		document.getElementById("nonConf").style.display="none";
-	}	
-}
-pword.onchange = checkPasses;
-cpswd.onkeyup = checkPasses;
-</script>
+	
 <body>
 	<div class="container">
 	<h1><img src="BPoC logo.png" width="227" height="135"></h1>
@@ -57,36 +43,42 @@ ini_set( 'display_errors' , 1 );
 
 
 	if(isset($_POST["submit"])){
-		
-		//Rabbit of Caerbannog
-		$client = new rabbitMQClient("brokerRabbitMQ.ini","testServer");
-		if (isset($argv[1]))
+		if($_POST["passwd"] == $_POST["confPass"];)
 		{
-		  $msg = $argv[1];
-		}
-		else
-		{
-		  $msg = "register.php testing";
-		}
+			//Rabbit of Caerbannog
+			$client = new rabbitMQClient("brokerRabbitMQ.ini","testServer");
+			if (isset($argv[1]))
+			{
+			  $msg = $argv[1];
+			}
+			else
+			{
+			  $msg = "register.php testing";
+			}
 
-		
-		$request = array();
-		$request['type'] = "register";
-		$request['username'] = $_POST["user"];
-		$request['password'] = $_POST["passwd"];
-		$request['email'] = $_POST["email"];
-		$request['message'] = $msg;
-		$response = $client->send_request($request); //Need a running rabbitMQServer.php
-		//$response = $client->publish($request);
-		echo $response['msg'];
 
-                if ($response['attempt'] == false){
-		//Registration Failed
-		  exit();
+			$request = array();
+			$request['type'] = "register";
+			$request['username'] = $_POST["user"];
+			$request['password'] = $_POST["passwd"];
+			$request['email'] = $_POST["email"];
+			$request['message'] = $msg;
+			$response = $client->send_request($request); //Need a running rabbitMQServer.php
+			//$response = $client->publish($request);
+			echo $response['msg'];
+
+			if ($response['attempt'] == false){
+			//Registration Failed
+			  exit();
+			}
+			else{
+				echo "<script>location.href='login.php'</script>";
+				//Take em to the login.
+			}
 		}
 		else{
-			echo "<script>location.href='login.php'</script>";
-			//Take em to the login.
+			//Registration Password does not match Confirmation Password
+			exit();
 		}
 	}
 
