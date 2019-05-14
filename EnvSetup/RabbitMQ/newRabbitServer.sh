539e265ab58bebcb5f51a77834b78bf6480595b2
Robert Facella <rob.facella@gmail.com>
2019-05-14 15:05:09 -0400

    cp sym link instead

--- EnvSetup/RabbitMQ/newRabbitServer.sh
+++ EnvSetup/RabbitMQ/newRabbitServer.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 #Get RabbitMQ Server started for testing.
-
 sudo apt install rabbitmq-server -y
 sudo ./importRabbitMQ.sh
 
 sudo apt-get install php -y
 sudo apt-get install php-amqp -y
 sudo apt-get install php-amqplib -y
-
-sudo cd /etc/php/7.0/cli/conf.d/
-sudo ln -s /etc/php/mods-available/amqp.ini
-
+#sudo cd /etc/php/7.0/cli/conf.d/
+#sudo ln -s /etc/php/mods-available/amqp.ini
+sudo cp amqp.ini /etc/php/7.0/cli/cpnf.d/amqp.ini
