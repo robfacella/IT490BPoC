3326b64e84481eb39753dd04e775f26b7611005e
Robert Facella <rob.facella@gmail.com>
2019-05-14 16:42:38 -0400

    Update cURL setup

--- EnvSetup/RabbitMQ/cURLRabbitMQSetup.sh
+++ EnvSetup/RabbitMQ/cURLRabbitMQSetup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+rabbitmqctl add_user test test
+rabbitmqctl set_user_tags test administrator
+###########
+
 curl -i -u guest:guest -H "content-type:application/json" -XPUT http://localhost:15672/api/vhosts/TESTHOST
 
 curl -i -u guest:guest -H "content-type:application/json" -XPUT -d'{"type":"topic","durable":true}' http://localhost:15672/api/exchanges/TESTHOST/TESTEXCHANGE
 
 curl -i -u guest:guest -H "content-type:application/json" -XPUT -d'{"auto_delete":false,"durable":true,"arguments":{},"node":"rabbit@rabbit"}'  http://localhost:15672/api/queues/TESTHOST/TESTQUEUE
+
+############
+rabbitmqctl set_permissions -p TESTHOST test ".*" ".*" ".*"
+############
