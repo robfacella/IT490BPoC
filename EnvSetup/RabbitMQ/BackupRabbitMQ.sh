cd9c712d8abd9e15c20c90a2aa5b26ab66ced03a
Robert Facella <rob.facella@gmail.com>
2019-05-14 14:20:25 -0400

    Backup /usr/lib/rabbitmq/*

--- EnvSetup/RabbitMQ/BackupRabbitMQ.sh
+++ EnvSetup/RabbitMQ/BackupRabbitMQ.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+#Backup RabbitMQ Configuration from the Dev Server to the Git Repo
 cp * from /usr/lib/rabbitmq to /EnvSetup/RabbitMQ/librabbitmq
+echo "Copying files from /usr/lib/rabbitmq to this repository's EnvSetup/RabbitMQ/librabbitmq directory"
+cp -r /usr/lib/rabbitmq/. librabbitmq
+echo "Copied..."
+
+echo "DONE."
