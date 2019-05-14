81671b85c10c9f44b70f4e37028d684942c8c51c
Robert Facella <rob.facella@gmail.com>
2019-05-14 16:11:55 -0400

    curl notes

--- EnvSetup/RabbitMQ/newRabbitServer.sh
+++ EnvSetup/RabbitMQ/newRabbitServer.sh
@@ -11,3 +11,6 @@ sudo apt-get install php-amqplib -y
 #sudo cd /etc/php/7.0/cli/conf.d/
 #sudo ln -s /etc/php/mods-available/amqp.ini
 sudo cp amqp.ini /etc/php/7.0/cli/conf.d/amqp.ini
+sudo apt install curl -y
+
+
