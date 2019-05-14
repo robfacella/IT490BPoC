59c391d8a86de446da056f15f6dbf93c6683418c
Robert Facella <rob.facella@gmail.com>
2019-05-14 14:28:10 -0400

    import from here

--- EnvSetup/RabbitMQ/importRabbitMQ.sh
+++ EnvSetup/RabbitMQ/importRabbitMQ.sh
@@ -1,2 +1,13 @@
-cp * from /EnvSetup/RabbitMQ/librabbitmq to /usr/lib/rabbitmq
+#!/bin/bash
+
+echo "Going to need sudo for this script. To reset the service 'rabbitmq-server'." #or will we...
+echo "Clearing local /usr/lib/rabbitmq"
+rm -r /usr/lib/rabbitmq*
+
+echo "Copying files from this repository's RabbitMQ/librabbitmq directory to /usr/lib/rabbitmq"
+cp -r librabbitmq/. /usr/lib/rabbitmq
+echo "Copied..."
+
+echo "Restarting rabbitmq server..."
 service rabbitmq-server restart
+echo "DONE."
