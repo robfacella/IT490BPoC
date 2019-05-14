bc2ecaf32f65e5794e22c3a848ee481b9dd0ea4b
Robert Facella <rob.facella@gmail.com>
2019-05-14 14:53:49 -0400

    RabbitMQ Setup PreReqs

--- EnvSetup/RabbitMQ/newRabbitServer.sh
+++ EnvSetup/RabbitMQ/newRabbitServer.sh
@@ -2,5 +2,13 @@
 
 #Get RabbitMQ Server started for testing.
 
-sudo apt install rabbitmq-server
+sudo apt install rabbitmq-server -y
 sudo ./importRabbitMQ.sh
+
+sudo apt-get install php -y
+sudo apt-get install php-amqp -y
+sudo apt-get install php-amqplib -y
+
+sudo cd /etc/php/7.0/cli/conf.d/
+sudo ln -s /etc/php/mods-available/amqp.ini
+
