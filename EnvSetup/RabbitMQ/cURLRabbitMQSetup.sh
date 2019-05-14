c2f746b14648bc666c8636ff052e478d1f2f7e81
Robert Facella <rob.facella@gmail.com>
2019-05-14 16:33:47 -0400

    Update cURLRabbitMQSetup.sh

--- EnvSetup/RabbitMQ/cURLRabbitMQSetup.sh
+++ EnvSetup/RabbitMQ/cURLRabbitMQSetup.sh
@@ -3,3 +3,5 @@
 curl -i -u guest:guest -H "content-type:application/json" -XPUT http://localhost:15672/api/vhosts/TESTHOST
 
 curl -i -u guest:guest -H "content-type:application/json" -XPUT -d'{"type":"topic","durable":true}' http://localhost:15672/api/exchanges/TESTHOST/TESTEXCHANGE
+
+curl -i -u guest:guest -H "content-type:application/json" -XPUT -d'{"auto_delete":false,"durable":true,"arguments":{},"node":"rabbit@rabbit"}'  http://localhost:15672/api/queues/TESTHOST/TESTQUEUE
