ef295a1de5c3402d677ece96b5c22b253105a81a
Robert Facella <rob.facella@gmail.com>
2019-05-15 15:35:56 -0400

    $HOME/IT490BPoC/ update

--- WWW/apache2Setup.sh
+++ WWW/apache2Setup.sh
@@ -12,7 +12,7 @@ echo "Clearing local /var/www/html"
 rm -r /var/www/html/*
 
 echo "Copying files from this repository's WWW/html directory to /var/www/html"
-cp -r html/. /var/www/html
+cp -r $HOME/IT490BPoC/WWW/html/. /var/www/html
 echo "Copied..."
 
 echo "Restarting apache2 server..."
