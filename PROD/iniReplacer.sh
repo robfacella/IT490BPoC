8afbb0980795b3634b059ea1f8b9496238b973a8
Robert Facella <rob.facella@gmail.com>
2019-05-15 20:03:46 -0400

    ini replacers

---
+++ PROD/iniReplacer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp $HOME/IT490BPoC/PROD/brokerRabbitMQ.ini $HOME/IT490BPoC/WWW/html/brokerRabbitMQ.ini
+cp $HOME/IT490BPoC/PROD/dbRabbitMQ.ini $HOME/IT490BPoC/WWW/html/dbRabbitMQ.ini
---
+++ QA/iniReplacer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp $HOME/IT490BPoC/QA/brokerRabbitMQ.ini $HOME/IT490BPoC/WWW/html/brokerRabbitMQ.ini
+cp $HOME/IT490BPoC/QA/dbRabbitMQ.ini $HOME/IT490BPoC/WWW/html/dbRabbitMQ.ini
